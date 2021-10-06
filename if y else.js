6dd71103d1b4a906c29b385dba4ba301b145d630
AngelMandujano <89949647+AngelMandujano@users.noreply.github.com>
2021-10-06 14:18:17 -0500

    if y else

---
+++ if y else.js
@@ -0,0 +1,25 @@
+var x = 300; // Variable para comparar
+var y = 20;
+var s = 30;
+if(x < y){
+    console.log(x + " es menor a " + y);
+}else{
+    console.log(x +" es mayor a " + y);
+}
+
+
+
+
+var a Donde ire = "tortilleria"
+var a Donde ire = "tienda"
+
+console.log ("paso 1 uno salir de casa");
+
+if(a Donde ire  == "tortilleria"){
+    console.log("Paso 2: caminar dos cuadras hacia la derecha");
+}else{
+    console.log("Paso 2:caminar 1 cuadra hacia la izquierda");
+}
+   console.log ("paso 3:entrar a la tortilleria");
+   console.log ("paso 4:pedir las tortillas");
+   console.log ("paso 5:pagar las tortillas")
